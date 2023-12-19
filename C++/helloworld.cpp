6e4688c9426796890fa415f8c4a4c78a8a6688a6
Andrew Apollo <andthetank@icloud.com>
2023-12-19 17:59:25 -0500

    Added new C++ folder and Hello World file

---
+++ C++/helloworld.cpp
@@ -0,0 +1,14 @@
+//Simple C++ program to display "Hello World"
+//Header file for input output functions 
+
+#include<iostream>
+
+using namespace std;
+
+//Main function
+//Where the execution of prgram begins 
+int main(){
+    cout<<"Hello World!";
+
+    return 0;
+}
